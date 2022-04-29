40d71cfb7b9b1fa22b73e7be0748933940997283
graffshumsky <100978279+graffshumsky@users.noreply.github.com>
2022-04-29 14:08:31 +0300

    залил проект на сервер
    изменения в коде

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -1,9 +1,20 @@
 "use strict"
 
+$(document).ready(function(){
+    
+    let options = {treshold: [1]};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = $('.element-animation');
+elements.each((i,el) => {
+    observer.observe(el);
+});
+    
+});
+
 let design1 = prompt("какой сайт вы хотите? (0- сайт-визитка- 10 000руб, 1- корпоративный сайт- 15 000руб, 2- лендинг- 25 000руб, 3- интернет-магазин- 40 000руб)");
 
 if(design1 == "0"){10000}
-else if(design1 == "1"){15000}
+else if (design1 == "1"){15000}
 else if(design1 == "2"){25000}
 else if(design1 == "3"){40000}
 else{alert("вы не ввели значение");
@@ -35,3 +46,48 @@ let result = +"design1" + +"design2" + +"design3";
           
 
 alert("ваша стоимость сайта" , "result");
+
+$('a[href^="#"]').click(function(){
+    let valHref = $(this).attr("href");
+    $('html, body').animate({scrollTop: $(valHref).offset().top - 80 + "px"});
+});
+
+function onEntry(entry){
+    entry.forEach(change => {
+        if (change.isIntersecting){
+            change.target.classList.add('show-animation');
+        }
+    });
+}
+
+$(function() {
+	
+		var target_block = $(".cifra"); // Ищем блок 
+		var blockStatus = true;
+		
+		$(window).scroll(function() {
+		
+			var scrollEvent = ($(window).scrollTop() > (target_block.position().top - $(window).height()));
+			
+			if(scrollEvent && blockStatus) {
+			
+				blockStatus = false; // Запрещаем повторное выполнение функции до следующей перезагрузки страницы.
+				
+				$({numberValue: 0}).animate({numberValue: 4000}, {
+				
+					duration: 1500, // Продолжительность анимации, где 500 - 0.5 одной секунды, то есть 500 миллисекунд 
+					easing: "linear",
+					
+					step: function(val) {
+					
+						$(".cifra").html(Math.ceil(val)); // Блок, где необходимо сделать анимацию
+						
+					}
+					
+				});
+				
+			}
+			
+		});
+		
+	});
